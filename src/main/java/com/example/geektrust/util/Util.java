d2cf85f9fb8111ba4130663a0ce2b404a1334706
sloganathan <sloganathan@deloitte.com>
2022-01-09 17:13:09 +0530

    puzzle

---
+++ src/main/java/com/example/geektrust/util/Util.java
@@ -0,0 +1,25 @@
+package com.example.geektrust.util;
+
+import com.example.geektrust.exception.WaterManagementException;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Util {
+    public static Integer getLitres(String ratio, int totalWaterConsumed, int index) {
+        List<Integer> ratios;
+        Integer sum;
+        try {
+            ratios = Arrays.stream(ratio.split(":")).map(Integer::parseInt).collect(Collectors.toList());
+            if (ratios.size() != 2) {
+                throw new WaterManagementException("Invalid Ratio");
+            }
+            sum = ratios.stream().mapToInt(Integer::intValue).sum();
+        }catch (Exception ex){
+            throw new WaterManagementException("Invalid Ratio");
+        }
+
+        return (totalWaterConsumed / sum) * ratios.get(index);
+    }
+}
