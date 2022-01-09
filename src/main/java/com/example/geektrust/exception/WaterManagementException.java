6c4063ff60f03a3d186cdbbe227993bdd4d070a6
sloganathan <sloganathan@deloitte.com>
2022-01-09 17:13:23 +0530

    puzzle

---
+++ src/main/java/com/example/geektrust/exception/WaterManagementException.java
@@ -0,0 +1,8 @@
+package com.example.geektrust.exception;
+
+public class WaterManagementException extends RuntimeException {
+
+    public WaterManagementException(String msg){
+        super(msg);
+    }
+}
